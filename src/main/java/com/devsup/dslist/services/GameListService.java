c73be2caf43ddce7dc020a957bfa8ebda9a25510
liv-inn <liviaavelino2002@gmail.com>
2025-01-22 18:36:44 -0300

    GET lists

---
+++ src/main/java/com/devsup/dslist/services/GameListService.java
@@ -0,0 +1,27 @@
+package com.devsup.dslist.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsup.dslist.dto.GameListDTO;
+import com.devsup.dslist.entities.GameList;
+import com.devsup.dslist.repositories.GameListRepository;
+
+
+@Service
+public class GameListService {
+
+	@Autowired
+	private GameListRepository gameListRepository;
+	
+	@Transactional(readOnly = true)
+	public List<GameListDTO> findAll(){
+		List<GameList> result = gameListRepository.findAll();
+		return result.stream().map(x -> new GameListDTO(x)).toList();
+		
+	}
+
+}
