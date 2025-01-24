2c217c22094fb4f73fe2ce67bcbb16343970336b
liv-inn <liviaavelino2002@gmail.com>
2025-01-24 11:59:31 -0300

    Deploy ready

--- src/main/java/com/devsup/dslist/dto/GameMinDTO.java
+++ src/main/java/com/devsup/dslist/dto/GameMinDTO.java
@@ -1,6 +1,7 @@
 package com.devsup.dslist.dto;
 
 import com.devsup.dslist.entities.Game;
+import com.devsup.dslist.projections.GameMinProjection;
 
 public class GameMinDTO {
 
@@ -21,6 +22,15 @@ public class GameMinDTO {
 		imgUrl = entity.getImgUrl();
 		shortDescription = entity.getShortDescription();
 	}
+	
+	public GameMinDTO(GameMinProjection projection) {
+		id = projection.getId();
+		title = projection.getTitle();
+		year = projection.getYear();
+		imgUrl = projection.getImgUrl();
+		shortDescription = projection.getShortDescription();
+	}
+
 
 
 	public Long getId() {
--- src/main/java/com/devsup/dslist/services/GameService.java
+++ src/main/java/com/devsup/dslist/services/GameService.java
@@ -3,12 +3,13 @@ package com.devsup.dslist.services;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
-import com.devsup.dslist.dto.GameMinDTO;
 import com.devsup.dslist.dto.GameDTO;
+import com.devsup.dslist.dto.GameMinDTO;
 import com.devsup.dslist.entities.Game;
+import com.devsup.dslist.projections.GameMinProjection;
 import com.devsup.dslist.repositories.GameRepository;
 
 
@@ -28,6 +29,13 @@ public class GameService {
 	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){
 		List<Game> result = gameRepository.findAll();
+		return result.stream().map(x -> new GameMinDTO(x)).toList();
+		
+	}
+	
+	@Transactional(readOnly = true)
+	public List<GameMinDTO> findByList(Long listId){
+		List<GameMinProjection> result = gameRepository.searchByList(listId);
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
 		
 	}
